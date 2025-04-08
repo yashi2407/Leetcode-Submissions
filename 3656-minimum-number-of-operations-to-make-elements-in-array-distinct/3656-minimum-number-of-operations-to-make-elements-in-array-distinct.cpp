75a59e68c3de1a5dfa9530f84221ddfaf85e910e
Yashika <60531590+yashi2407@users.noreply.github.com>
2025-04-08 13:31:59 -0400

    Time: 5 ms (37.88%), Space: 29.8 MB (22.87%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    // BRUTE FORCE APPROACH
-    // TC = O(N^2);
-    void removeFirst(vector<int>& nums){
-        if(nums.size()>=3){
-            // remove first three
-            nums.erase(nums.begin(), nums.begin() + 3);
-        }
-        else{
-            // nums become empty
-            nums.erase(nums.begin(), nums.begin() + nums.size());
-        }
-    }
+    // OPTIMIZED APPROACH
+    // TC = O(N);
     int minimumOperations(vector<int>& nums) {
-        map<int,int>mp;
-        int i = 0;
+        unordered_set<int> seen;
+        int start = 0;
         int count = 0;
-        while(i<nums.size()){
-            mp[nums[i]]++;
-            if(mp[nums[i]]>1){
-                // remove elements and start over!
-                removeFirst(nums);
-                i = 0;
-                mp.clear();
+
+        for (int i = start; i < nums.size(); i++) {
+            if (seen.count(nums[i])) {
+                // simulate removal of first 3 elements
                 count++;
-            }
-            else{
-                i++;
+                start += 3;
+                seen.clear();
+                i = start - 1; // -1 because the loop will increment i
+            } else {
+                seen.insert(nums[i]);
             }
         }
+
         return count;
     }
 };
