ff8de7a34a1322592c2da9ff2c59ec7ec8e4b24a
Yashika <60531590+yashi2407@users.noreply.github.com>
2025-05-27 00:46:50 -0400

    Time: 12 ms (20.21%), Space: 16.8 MB (6.56%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    bool util(string s, string p, int i, int j, vector<vector<int>> &dp){
-        if (i == s.length() && j == p.length()) return dp[i][j]= true;
+    unordered_map<string, bool> memo;
 
-        // If pattern is finished but string is not → fail
-        if (j == p.length()) return dp[i][j]= false;
+    bool util(int i, int j, string& s, string& p) {
+        string key = to_string(i) + "," + to_string(j);
+        if (memo.count(key)) return memo[key];
 
-        if (dp[i][j] != -1) return dp[i][j];
+        if (i == s.length() && j == p.length()) return memo[key] = true;
+        if (j == p.length()) return memo[key] = false;
+
+        bool match = (i < s.length()) && (s[i] == p[j] || p[j] == '.');
 
         if (j + 1 < p.length() && p[j + 1] == '*') {
-            // Case 1: Zero matching, I won't be matching any character at j+1 so. move to j+2
-            // eg- aaab and a* b so I am basically saying that a* for me ''.
-            if (util(s, p, i, j + 2, dp)) return dp[i][j]= true;
-
-            // Case 2: One or more occurrences s = "aaab",  p = "a*b"
-            if (i < s.length() && (s[i] == p[j] || p[j] == '.')) {
-                if (util(s, p, i + 1, j, dp)) return dp[i][j] = true;
-            }
-
-            return dp[i][j]= false; // if neither worked
+            // Try zero occurrences or one (if match)
+            return memo[key] = util(i, j + 2, s, p) || (match && util(i + 1, j, s, p));
         }
-        if (i < s.length() && (p[j] == s[i] || p[j] == '.')) {
-            return dp[i][j]= util(s, p, i + 1, j + 1, dp);
+
+        if (match) {
+            return memo[key] = util(i + 1, j + 1, s, p);
         }
-        return dp[i][j]= false;
+
+        return memo[key] = false;
     }
+
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.size()+1, vector<int>(p.size()+1, -1));
-        return util(s,p,0,0,dp);
+        return util(0, 0, s, p);
     }
 };
