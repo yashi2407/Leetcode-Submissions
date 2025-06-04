399d581dc3eac6e3a49280c8e25f5235e8c9cebd
Yashika <60531590+yashi2407@users.noreply.github.com>
2025-06-04 11:03:25 +0530

    Time: 12 ms (35.22%), Space: 22.5 MB (13.8%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,76 @@
+class Solution {
+public:
+    void setNsr(vector<int>& heights,vector<int>&nsr){
+        stack<pair<int,int>>st;
+        for(int i= heights.size()-1;i>=0;i--){
+            if(st.size()==0){
+                nsr[i]=heights.size();
+            }
+            else if(st.top().first<heights[i]){
+                nsr[i]=st.top().second;
+            }
+            else {
+                while(!st.empty() && st.top().first>=heights[i]){
+                    st.pop();
+                }
+                if(st.size()==0){
+                    nsr[i]=heights.size();
+                }
+                else{
+                    nsr[i]=st.top().second;
+                }
+            }
+            st.push({heights[i],i});
+        }
+    }
+    void setNsl(vector<int>& heights,vector<int>&nsl){
+        stack<pair<int,int>>st;
+        for(int i=0;i<heights.size();i++){
+            if(st.size()==0){
+                nsl[i]=-1;
+            }
+            else if(st.top().first<heights[i]){
+                nsl[i]=st.top().second;
+            }
+            else {
+                while(!st.empty() && st.top().first>=heights[i]){
+                    st.pop();
+                }
+                if(st.size()==0){
+                    nsl[i]=-1;
+                }
+                else{
+                    nsl[i]=st.top().second;
+                }
+            }
+            st.push({heights[i],i});
+        }
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        vector<int>nsl(heights.size(),-1);
+        setNsl(heights,nsl);
+        vector<int>nsr(heights.size(),-1);
+        setNsr(heights,nsr);
+        int ans = INT_MIN;
+        for(int i =0;i<heights.size();i++){
+            ans=max(ans,(nsr[i]-nsl[i]-1)*heights[i]);
+        }
+        return ans;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int ans = INT_MIN;
+        if (matrix.empty() || matrix[0].empty()) return 0;
+        vector<int>heights(matrix[0].size(),0);
+        for(int i =0;i<matrix.size();i++){
+            for(int j = 0;j<matrix[0].size();j++){
+                if (matrix[i][j] == '1') {
+                    heights[j] += 1;
+                } else {
+                    heights[j] = 0;  // Reset if we hit a 0
+                }
+            }
+            ans=max(ans,largestRectangleArea(heights));
+        }
+        return ans;
+    }
+};
