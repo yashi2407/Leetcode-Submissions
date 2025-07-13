25d1b6f9c4ed2a49d6cd8a247565af0685b90af5
Yashika <60531590+yashi2407@users.noreply.github.com>
2025-07-13 11:31:02 +0530

    Time: 333 ms (6.11%), Space: 15.6 MB (42.52%) - LeetHub

---
+++ 2061-painting-a-grid-with-three-different-colors/2061-painting-a-grid-with-three-different-colors.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<string>columnStates;
+    int M = 1e9+7;
+    unordered_map<string, int> stateToIndex;
+    vector<vector<int>> dp;
+    void generateColumnsStates(int currentRow, char prevColour,int m, string current){
+        if(currentRow==m){
+            columnStates.push_back(current);
+            return;
+        }
+        // loop over all the choices
+        for(char ch :{'R','G', 'B'}){
+            if(ch == prevColour){
+                continue;
+            }
+            generateColumnsStates(currentRow+1,ch,m,current+ch);
+        }
+    }
+
+    int solve(int currentCol, int n , int prevStateIndex){
+        if(currentCol == n){
+            return 1;
+        }
+        if(dp[currentCol][prevStateIndex]!=-1){
+            return dp[currentCol][prevStateIndex];
+        }
+        int ways = 0;
+        string prevState = columnStates[prevStateIndex];
+        for(int i=0;i<columnStates.size();i++){
+            string nextState = columnStates[i];
+            bool isValid = true;
+            for(int j=0;j<prevState.size();j++){
+                if(prevState[j]==nextState[j]){
+                    isValid = false;
+                    break;
+                }
+            }
+            if(isValid){
+                ways = (ways + solve(currentCol+1,n,i)) % M;
+            }
+        }
+        return dp[currentCol][prevStateIndex]= ways;
+    }
+    int colorTheGrid(int m, int n) {
+        generateColumnsStates(0,'#',m,"");
+        int k = columnStates.size();
+        dp.assign(n, vector<int>(k, -1));
+
+        for (int i = 0; i < columnStates.size(); i++) {
+            stateToIndex[columnStates[i]] = i;
+        }
+        int result = 0;
+        for(int i=0;i<columnStates.size();i++){
+            result = (result + solve(1, n, i)) % M;
+        }
+        return result % M;
+    }
+};
