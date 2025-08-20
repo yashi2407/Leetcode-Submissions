2c6b5cd7d4b8847924cfe8bf1710dfb0f280ab22
Yashika <60531590+yashi2407@users.noreply.github.com>
2025-08-20 20:47:31 +0530

    Time: 9 ms (58.48%), Space: 11.4 MB (66.42%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,38 +1,40 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        map<char,int> mp;
+        unordered_map<char,int>mp;
         for(int i = 0;i<t.size();i++){
             mp[t[i]]++;
         }
-        int count = mp.size(), i = 0,j=0;
-        string ans = "";
-        int minLen = INT_MAX;
+        int count = mp.size();
+        int i=0,j=0;
+        int minLength = INT_MAX;
         int startIndex = 0;
         while(j<s.size()){
+            // do calculations for j
             if(mp.find(s[j])!=mp.end()){
                 mp[s[j]]--;
-                if (mp[s[j]] == 0) count--;
-            }
-            // start removing to optimize the string
-            while(count == 0){
-                int currentLength = j-i+1;
-                if(currentLength<minLen){
-                    // possible candidate
-                    minLen = j - i + 1;
-                    startIndex = i;
+                if(mp[s[j]]==0){
+                    count--;
                 }
+            }
+            // when we hit the count = 0, possible answer found
+            while(count == 0){
+                int currentLength = j - i + 1;
+                if (currentLength < minLength) {
+                    minLength = currentLength;
+                    startIndex = i;  // <- capture start
+                }
+                // remove the calcualtions of i till count == 0
                 if(mp.find(s[i])!=mp.end()){
                     mp[s[i]]++;
-                    if(mp[s[i]]>0){
+                    if(mp[s[i]]== 1){
                         count++;
                     }
                 }
                 i++;
             }
             j++;
-            
         }
-        return minLen == INT_MAX ? "" : s.substr(startIndex, minLen);
+        return (minLength == INT_MAX) ? "" : s.substr(startIndex, minLength);
     }
 };
