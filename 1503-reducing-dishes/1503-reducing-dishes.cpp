a2be1592a4b10f153c60d774754b4ed9398dba13
Yashika <60531590+yashi2407@users.noreply.github.com>
2025-08-20 23:26:54 +0530

    Time: 22 ms (30.17%), Space: 36.5 MB (6.26%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -14,9 +14,26 @@ public:
 
     int maxSatisfaction(vector<int>& satisfaction) {
         sort(satisfaction.begin(), satisfaction.end());
+        // int n = (int)satisfaction.size();
+        // // // dishIndex can go up to n, so allocate n+2 to be safe
+        // vector<vector<int>> dp(n + 1, vector<int>(n + 2, INT_MIN));
+        // return util(satisfaction, 0, 1, dp);
+
         int n = (int)satisfaction.size();
-        // dishIndex can go up to n, so allocate n+2 to be safe
-        vector<vector<int>> dp(n + 1, vector<int>(n + 2, INT_MIN));
-        return util(satisfaction, 0, 1, dp);
+
+        // dp[i][dishIndex] = best from i..n-1 when next picked dish has index = dishIndex
+        vector<vector<long long>> dp(n + 1, vector<long long>(n + 2, 0));
+
+        // reverse the recursion order:
+        // i goes from n-1 down to 0
+        // dishIndex goes from n down to 1
+        for (int i = n - 1; i >= 0; --i) {
+            for (int dishIndex = n; dishIndex >= 1; --dishIndex) {
+                long long pick    = 1LL * satisfaction[i] * dishIndex + dp[i + 1][dishIndex + 1];
+                long long notPick = dp[i + 1][dishIndex];
+                dp[i][dishIndex]  = max(pick, notPick);
+            }
+        }
+        return (int)dp[0][1];
     }
 };
