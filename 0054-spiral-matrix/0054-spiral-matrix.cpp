e39b4e5ef807f1641a3f6cd8918cc30c27aed0bf
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-04 13:34:59 -0500

    Time: 0 ms (100%), Space: 9.4 MB (14.12%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,59 +1,39 @@
 class Solution {
 public:
-    void goRight(vector<vector<int>>& matrix,vector<int> &ans, int &i, int &j,vector<vector<int>> &vis){
-        while(j<matrix[0].size() && !vis[i][j]){
-            ans.push_back(matrix[i][j]);
-            vis[i][j]= 1;
-            j++;
-        }
-        j--;
-    }
-    void goDown(vector<vector<int>>& matrix,vector<int> &ans, int &i, int &j,vector<vector<int>> &vis){
-        while(i<matrix.size() && !vis[i][j]){
-            ans.push_back(matrix[i][j]);
-            vis[i][j]= 1;
-            i++;
-        }
-        i--;
-    }
-    void goLeft(vector<vector<int>>& matrix,vector<int> &ans, int &i, int &j,vector<vector<int>> &vis){
-        while(j>=0 && !vis[i][j]){
-            ans.push_back(matrix[i][j]);
-            vis[i][j]= 1;
-            j--;
-        }
-        j++;
-    }
-    void goUp(vector<vector<int>>& matrix,vector<int> &ans, int &i, int &j,vector<vector<int>> &vis){
-        while(i>=0 && !vis[i][j]){
-            ans.push_back(matrix[i][j]);
-            vis[i][j]= 1;
-            i--;
-        }
-        i++;
-    }
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
         vector<int> ans;
-        vector<vector<int>> vis(matrix.size(),vector<int>(matrix[0].size(),0));
-        int i=0;int j=0;
-        int total = matrix.size() * matrix[0].size();
 
-        while ((int)ans.size() < total) {
-            goRight(matrix, ans, i, j, vis);
-            if ((int)ans.size() == total) break;
-            i++; // move to start of down
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
 
-            goDown(matrix, ans, i, j, vis);
-            if ((int)ans.size() == total) break;
-            j--; // move to start of left
+        while (top <= bottom && left <= right) {
 
-            goLeft(matrix, ans, i, j, vis);
-            if ((int)ans.size() == total) break;
-            i--; // move to start of up
+            // 1) Go Right
+            for (int j = left; j <= right; j++)
+                ans.push_back(matrix[top][j]);
+            top++;
 
-            goUp(matrix, ans, i, j, vis);
-            if ((int)ans.size() == total) break;
-            j++; // move to start of right
+            // 2) Go Down
+            for (int i = top; i <= bottom; i++)
+                ans.push_back(matrix[i][right]);
+            right--;
+
+            // 3) Go Left
+            if (top <= bottom) {
+                for (int j = right; j >= left; j--)
+                    ans.push_back(matrix[bottom][j]);
+                bottom--;
+            }
+
+            // 4) Go Up
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    ans.push_back(matrix[i][left]);
+                left++;
+            }
         }
 
         return ans;
