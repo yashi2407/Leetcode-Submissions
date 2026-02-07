29245b566e57371ea945bb4bb4c7a8fe50bf0ec2
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-07 11:09:43 -0500

    Time: 0 ms (100%), Space: 14.1 MB (57.75%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     void solve(vector<int>& candidates, int target ,vector<vector<int>>&ans,vector<int>&currentCombinations, int currentIndex){
-        if(currentIndex>=candidates.size()){
             if(target == 0){
                 ans.push_back(currentCombinations);
+                return;
             }
-            return;
-        }
+            // try all valid positions
+            for(int i = currentIndex;i<candidates.size();i++){
+                if(candidates[i]>target){
+                    break;
+                }
+                currentCombinations.push_back(candidates[i]);
+                solve(candidates, target-candidates[i], ans,currentCombinations,i);
+                currentCombinations.pop_back();
 
-        // if possible to pick!
-        if(candidates[currentIndex]<=target){
-            //choose the element
-            currentCombinations.push_back(candidates[currentIndex]);
-            solve(candidates,target-candidates[currentIndex],ans,currentCombinations, currentIndex);
-            currentCombinations.pop_back();
         }
-        solve(candidates,target,ans,currentCombinations, currentIndex+1);
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<vector<int>> ans;
         vector<int> currentCombinations;
+        sort(candidates.begin(), candidates.end());
         solve(candidates,target,ans,currentCombinations, 0);
         return ans;
     }
