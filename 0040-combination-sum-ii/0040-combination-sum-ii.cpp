f67c1fa3fdddd8c8856ad9b1954836caa427bd48
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-07 11:04:34 -0500

    Time: 0 ms (100%), Space: 14.1 MB (38.88%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    void util(vector<int>& candidates, int target, vector<vector<int>> &ans, vector<int>&combination, int index){
+    void util(vector<int>&candidates, int target, vector<vector<int>> &ans, vector<int>&combination, int index){
         if(target == 0){
             ans.push_back(combination);
             return;
         }
         for(int i = index;i<candidates.size();i++){
-            if(i>index && candidates[i-1] == candidates[i]){
-                continue;
-            }
-            if(target<candidates[i]){
+            if (i > index && candidates[i] == candidates[i-1]) continue;
+            if(candidates[i]>target){
                 break;
             }
             combination.push_back(candidates[i]);
-            util(candidates,target-candidates[i],ans,combination,i+1);
+            util(candidates, target-candidates[i], ans,combination,i+1);
             combination.pop_back();
+
         }
+        
     }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
         sort(candidates.begin(), candidates.end());
