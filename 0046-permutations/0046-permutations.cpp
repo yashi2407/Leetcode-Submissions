6136d97bd617f7aa7750f5b56818c6a0c64ca7c2
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-14 15:41:01 -0500

    Time: 0 ms (100%), Space: 10.3 MB (97.97%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-    void util(vector<int>& nums,vector<vector<int>> &ans, int index){
+    void util(vector<int>& nums,vector<vector<int>>&ans, int index){
         if(index>=nums.size()){
             ans.push_back(nums);
             return;
         }
-        for(int i=index;i<nums.size();i++){
-            swap(nums[index],nums[i]);
+        for(int i = index;i<nums.size();i++){
+            swap(nums[i],nums[index]);
             util(nums,ans,index+1);
-            swap(nums[index],nums[i]);
+            swap(nums[i],nums[index]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>> ans;
+        vector<vector<int>>ans;
         util(nums,ans,0);
         return ans;
     }
