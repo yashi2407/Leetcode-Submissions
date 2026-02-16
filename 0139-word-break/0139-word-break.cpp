8fb3bfa33ba6bc75b008eb6afec2d10b8b183726
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-16 13:31:51 -0500

    Time: 15 ms (28.51%), Space: 18.5 MB (15.97%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool util(string s,unordered_map<string,int>&mp,unordered_map<string,bool>&memo){
-        if(s.size()==0){
+    bool util(string s,vector<string>& wordDict, int currentIndex, vector<int> &dp){
+        // base case!
+        if(currentIndex>=s.size()){
             return true;
         }
-        if(memo.find(s)!=memo.end()){
-            return memo[s];
-        }
-        for(int i = 1;i<=s.size();i++){
-            string temp = s.substr(0,i);
-            cout<<temp<<" ";
-            if(mp.find(temp)!=mp.end()){
-                if(util(s.substr(i),mp,memo)){
-                    return memo[s]= true;
+        if (dp[currentIndex] != -1) return dp[currentIndex];
+        // from currentIndex, I can try breaking at all the valid positions
+        for(int i = currentIndex;i<s.size();i++){
+            string temp = s.substr(currentIndex, i-currentIndex+1);
+            auto it = find(wordDict.begin(), wordDict.end(), temp);
+            if (it != wordDict.end()) {
+                if(util(s,wordDict,i+1,dp)){
+                    return dp[currentIndex] = true;
                 }
             }
         }
-        return memo[s] = false;
+        return dp[currentIndex]= false;
     }
     bool wordBreak(string s, vector<string>& wordDict) {
-        unordered_map<string,int>mp;
-        for(int i=0;i<wordDict.size();i++){
-            mp[wordDict[i]]++;
-        }
-        unordered_map<string,bool> memo;
-        return util(s,mp,memo);
+        vector<int> dp(s.size(), -1);
+        return util(s,wordDict,0,dp);
     }
 };
