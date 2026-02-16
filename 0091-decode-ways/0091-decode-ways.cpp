f0406db2d5a25003c6024977a19821f927a38df8
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-16 13:43:53 -0500

    Time: 4 ms (8.58%), Space: 16.3 MB (5.09%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int util(string s, int index,vector<int>&dp){
-        if(index>=s.size()){
-            return dp[index]= 1;
+    int util(string s, int currentIndex,vector<int>&dp){
+        // base condition
+        if(currentIndex>=s.size()){
+            return 1;
         }
-        if(s[index] == '0') return dp[index] = 0;
-        // two choices, take it alone or club with the second if possible
-        if(dp[index]!=-1){
-            return dp[index];
+        if(s[currentIndex] == '0'){
+            return 0;
+        }
+        if(dp[currentIndex]!=-1){
+            return dp[currentIndex];
         }
         int ans = 0;
-        int num = stoi(s.substr(index, 2));
-        if (num >= 10 && num <= 26) {
-            ans += util(s, index + 2,dp);
+        int num = stoi(s.substr(currentIndex, 2));
+        if(num>=10 && num<=26){
+            // we can choose two index
+            ans += util(s,currentIndex+2,dp);
         }
-        ans+=util(s,index+1,dp);
-        return dp[index]= ans;
+        // we just goto next index
+        ans += util(s,currentIndex+1, dp);
+        return dp[currentIndex] = ans;
+
     }
     int numDecodings(string s) {
         vector<int>dp(s.size()+1,-1);
