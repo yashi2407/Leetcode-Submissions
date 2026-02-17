b21364ed4ea84ec178a95e1633a742fa3448917f
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-17 12:36:47 -0500

    Time: 0 ms (100%), Space: 15.2 MB (34.39%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,31 +1,34 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low=0,high = nums.size()-1;
+        int low = 0; int high = nums.size()-1;
         while(low<=high){
-            int mid = (low+high)/2;
-            if(nums[mid]==target){
+            int mid = (high - low)/2 + low;
+            // if mid is the target return that
+            if(nums[mid] == target){
                 return mid;
             }
-            // check if left sorted
-            if (nums[low] <= nums[mid]) {
-                //element inside the sorted half
-                if (target >= nums[low] && target < nums[mid]) {
-                    high = mid - 1;
-                } else {
-                    low = mid + 1;//element not inside the sorted half hence in right half
+            // check if left side sorted
+            if(nums[low]<=nums[mid]){
+                // check if target lies in that boundary
+                if(nums[low] <= target && target < nums[mid]){
+                    high = mid-1;
+                }
+                else {
+                    low = mid+1;
                 }
             }
-            // Right side is sorted
-            else {
-                if (target > nums[mid] && target <= nums[high]) {
-                    low = mid + 1;
-                } else {
-                    high = mid - 1;
+            // else right side is sorted
+            else{
+                // check if target lies in that boundary
+                if(nums[mid] < target && target <= nums[high]){
+                    low = mid+1;
+                }
+                else {
+                    high = mid-1;
                 }
             }
         }
         return -1;
-        
     }
 };
