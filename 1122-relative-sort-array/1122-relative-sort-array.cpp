c05c52b85fc129ca9ec03149292db2cb450d949b
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-02-25 14:10:01 -0500

    Time: 1 ms (30.76%), Space: 11.2 MB (42.76%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.cpp
+++ 1122-relative-sort-array/1122-relative-sort-array.cpp
@@ -1,24 +1,44 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        sort(arr1.begin(),arr1.end(),[&arr2](int a , int b){
-            auto it1 = find(arr2.begin(), arr2.end(), a);
-            auto it2 = find(arr2.begin(), arr2.end(), b);
-            // found both 1 and 2
-            if (it1 != arr2.end() && it2 != arr2.end()){
-                // a should appear before b if it1 < it2
-                return (it1 - arr2.begin()) < (it2 - arr2.begin());
+        unordered_map<int,int>mp;
+        for(int i=0;i<arr2.size();i++){
+            mp[arr2[i]] = i;
+        }
+
+        sort(arr1.begin(),arr1.end(), [&mp](int a, int b){
+            auto it1 = mp.find(a);
+            auto it2 = mp.find(b);
+            if(it1 != mp.end() && it2!=mp.end()){
+                return it1->second<it2->second;
             }
-            // found a
-            else if(it1 != arr2.end()){
+            else if(mp.find(a) != mp.end()){
                 return true;
             }
-            else if(it2 != arr2.end()){
+            else if(mp.find(b) != mp.end()){
                 return false;
             }
             return a<b;
-            
         });
+
+        // sort(arr1.begin(),arr1.end(),[&arr2](int a , int b){
+        //     auto it1 = find(arr2.begin(), arr2.end(), a);
+        //     auto it2 = find(arr2.begin(), arr2.end(), b);
+        //     // found both 1 and 2
+        //     if (it1 != arr2.end() && it2 != arr2.end()){
+        //         // a should appear before b if it1 < it2
+        //         return (it1 - arr2.begin()) < (it2 - arr2.begin());
+        //     }
+        //     // found a
+        //     else if(it1 != arr2.end()){
+        //         return true;
+        //     }
+        //     else if(it2 != arr2.end()){
+        //         return false;
+        //     }
+        //     return a<b;
+            
+        // });
 
         return arr1;
     }
