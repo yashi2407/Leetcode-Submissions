da922afb0d842bbbafcf671326b50a367257b98d
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-03-25 11:21:13 -0400

    Time: 0 ms (100%), Space: 9.4 MB (51.55%) - LeetHub

--- 0394-decode-string/0394-decode-string.cpp
+++ 0394-decode-string/0394-decode-string.cpp
@@ -3,44 +3,42 @@ public:
     string decodeString(string s) {
         stack<char>st;
         int i=0;
-        for(int i=0;i<s.size();i++){
-            if(s[i] == ']'){
-                // will do something
-                // first get the string
-                string temp="";
-                while(st.top()!='['){
-                    temp+=st.top();
-                    st.pop();
-                }
-                st.pop(); // for ']'
-                // now get the number till which we have to repeat
-                string number = "";
-                while(!st.empty() && isdigit(st.top())){
-                    number+=st.top();
-                    st.pop();
-                }
-                reverse(number.begin(),number.end());
-                int num = stoi(number);
-                string newString = "";
-                for(int i=1;i<=num;i++){
-                    newString+=temp;
-                }
-                // put in stack from back!
-                for(int j = newString.size() - 1; j >= 0; j--){
-                    st.push(newString[j]);
-                }
-
-            }
-            else{
+        while(i!=s.size()){
+            if(s[i]!= ']'){
                 st.push(s[i]);
+                i++;
+            }
+            else {
+                string toRepeat = "";
+                while(st.top()!= '['){
+                    toRepeat += st.top();
+                    st.pop();                
+                }
+                reverse(toRepeat.begin(), toRepeat.end());
+                st.pop();
+                // now get how many times to repeat!
+                string repeatNum = "";
+                while(!st.empty() && isdigit(st.top())){
+                    repeatNum+=st.top();
+                    st.pop();
+                }
+                reverse(repeatNum.begin(),repeatNum.end());
+                string tempString = "";
+                for(int i=0;i<stoi(repeatNum);i++){
+                    tempString+=toRepeat;
+                }
+                for (char ch : tempString) {
+                    st.push(ch);
+                }
+                i++;
             }
         }
-        string ans;
-        while(!st.empty()){
-            ans+=st.top();
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
             st.pop();
         }
-        reverse(ans.begin(),ans.end());
+        reverse(ans.begin(), ans.end());
         return ans;
     }
 };
