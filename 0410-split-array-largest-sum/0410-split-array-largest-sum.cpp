aacf0d48ade6b0db507631addac7f9815ea2304a
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-03-30 19:34:48 -0400

    Time: 0 ms (100%), Space: 10.5 MB (64.32%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -17,23 +17,19 @@ public:
     int splitArray(vector<int>& nums, int k) {
         int minElement = *max_element(nums.begin(),nums.end());
         int maxElement = 0;
-        int ans = -1;
-        for(int i = 0;i<nums.size();i++){
+        for(int i=0;i<nums.size();i++){
             maxElement += nums[i];
         }
-        if (k == 1) return maxElement;
-        // if (k == nums.size()) return *max_element(nums.begin(),nums.end());
-        while(minElement <= maxElement){
-            int mid = (minElement+maxElement)/2;
-            int countSubArray = countSubarrays(nums,mid);
-            if(countSubArray > k){
-                minElement = mid+1;
-            } else {
-                // move towards min element in case you find an answer!
-                ans = mid;
-                maxElement = mid-1;
+        int ans = 0;
+        while(minElement<=maxElement){
+            int sum = (minElement+ maxElement)/2;
+            if(countSubarrays(nums,sum)>k){
+                minElement = sum + 1;
             }
-
+            else {
+                ans = sum;
+                maxElement = sum - 1;
+            }
         }
         return ans;
     }
