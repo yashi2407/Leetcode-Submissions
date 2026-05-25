798ed51b214bd8dbdf9b12cbf35f1156fdf4e3b4
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-05-25 12:41:43 -0400

    Time: 0 ms (100%), Space: 20.8 MB (75.3%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int,int>>locations;
-        //O(mn)
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j] == 0){
-                    locations.push_back({i,j});
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> rows(m, 0);
+        vector<int> cols(n, 0);
+
+        // mark rows and cols
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(matrix[i][j] == 0) {
+                    rows[i] = 1;
+                    cols[j] = 1;
                 }
             }
         }
-        unordered_set<int>rowsChanged;
-        unordered_set<int>colChanged;
 
-        // iterate over the loctions and set them
-        for(int i=0;i<locations.size();i++){
-            int row = locations[i].first;
-            int col = locations[i].second;
-
-            if(rowsChanged.find(row) == rowsChanged.end()){
-                // change row zero
-                for(int j = 0;j<matrix[0].size();j++){
-                    matrix[row][j] = 0;
+        // set zeroes
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
                 }
-                rowsChanged.insert(row);
-            }
-            if(colChanged.find(col) == colChanged.end()){
-                // change col!!
-                for(int j=0;j<matrix.size();j++){
-                    matrix[j][col] = 0;
-                }
-                colChanged.insert(col);
             }
         }
     }
