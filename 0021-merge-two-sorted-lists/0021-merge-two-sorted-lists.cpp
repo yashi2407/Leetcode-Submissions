2d10ca66c8ab67b09f55159305019d03d1d576d4
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-05-31 12:47:46 -0400

    Time: 0 ms (100%), Space: 19.6 MB (26.25%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -26,15 +26,11 @@ public:
                 list2=list2->next;
             }
         }
-        while(list1){
+        if(list1){
             temp->next = list1;
-            temp=temp->next;
-            list1=list1->next;
         }
-        while(list2){
+        if(list2){
             temp->next = list2;
-            temp = temp->next;
-            list2=list2->next;
         }
         return newHead->next;
     }
