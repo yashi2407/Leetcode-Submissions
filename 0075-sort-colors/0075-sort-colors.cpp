8a9b28538844ec92d9447e54a97d42b7f31a1a74
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-06-22 20:33:09 -0400

    Time: 0 ms (100%), Space: 11.6 MB (78.31%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        for(int i = 1;i<nums.size();i++){
-            int k = i,j=i-1;
-            while(j>=0 && nums[j]>nums[k]){
-                swap(nums[j],nums[k]);
-                j--; 
-                k--;
+        int start = 0;
+        int end = nums.size()-1;
+        int i = 0;// current position!
+        while (i <= end){
+            if(nums[i] == 2){
+                swap(nums[i],nums[end]);
+                end--;
+            }
+            else if(nums[i] == 0){
+                swap(nums[i],nums[start]);
+                start++;
+                i++;
+            }
+            else {
+                i++;
             }
         }
     }
