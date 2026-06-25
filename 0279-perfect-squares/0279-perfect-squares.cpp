13988101379e25143ae4e21dc1a4a279bb75f8d8
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-06-24 20:07:14 -0400

    Time: 83 ms (30.25%), Space: 14 MB (30.98%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int util(int n, vector<int>& dp) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        int i = 1;
+        int ans = 123456;
+        while (i * i <= n) {
+            int choice = i * i;
+            ans = min(ans, 1 + util(n - choice, dp));
+            i++;
+        }
+
+        return dp[n] = ans;
+    }
+
+    int numSquares(int n) {
+        vector<int> dp(n + 1, -1);
+        return util(n, dp);
+    }
+};
