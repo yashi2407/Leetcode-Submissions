ad5dccf7dd0c23476bc1a5c359c2d0213588ff0c
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-06-28 13:42:33 -0400

    Time: 0 ms (100%), Space: 8.7 MB (93.57%) - LeetHub

--- 0299-bulls-and-cows/0299-bulls-and-cows.cpp
+++ 0299-bulls-and-cows/0299-bulls-and-cows.cpp
@@ -2,32 +2,24 @@ class Solution {
 public:
     string getHint(string secret, string guess) {
         int bulls = 0;
-        for(int i=0;i<secret.size();i++){
-            if(secret[i] == guess[i]){
-                bulls++;
-                secret.erase(secret.begin()+ i);
-                guess.erase(guess.begin()+ i);
-                i--;
+        int cows = 0;
 
+        vector<int> secretCount(10, 0);
+        vector<int> guessCount(10, 0);
+
+        for (int i = 0; i < secret.size(); i++) {
+            if (secret[i] == guess[i]) {
+                bulls++;
+            } else {
+                secretCount[secret[i] - '0']++;
+                guessCount[guess[i] - '0']++;
             }
         }
-        cout<<secret<<" "<<guess;
-        int cows = 0;
-        //  now we just need to count the same digits in secret and guess!
-        vector<int>count1(10,0);
-        vector<int>count2(10,0);
-        for (char c : secret) {
-            count1[c - '0']++;
+
+        for (int i = 0; i < 10; i++) {
+            cows += min(secretCount[i], guessCount[i]);
         }
 
-        for (char c : guess) {
-            count2[c - '0']++;
-        }
-        for(int i=0;i<10;i++){
-            cows+= min(count1[i],count2[i]);
-        }
-        string ans = to_string(bulls)+ "A" + to_string(cows) + "B" ;
-        return ans;
-
+        return to_string(bulls) + "A" + to_string(cows) + "B";
     }
 };
