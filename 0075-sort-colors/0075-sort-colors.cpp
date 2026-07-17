9c6d86a13d17cc7821d12d2b7ecc0124c5978080
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-07-16 22:13:56 -0400

    Time: 0 ms (100%), Space: 11.5 MB (79.1%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,17 +1,17 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int start = 0;
-        int end = nums.size()-1;
-        int i = 0;// current position!
-        while (i <= end){
+        int zeroIndex = 0;
+        int twoIndex = nums.size()-1;
+        int i=0;
+        while(i<=twoIndex){ // why this while condition???
             if(nums[i] == 2){
-                swap(nums[i],nums[end]);
-                end--;
+                swap(nums[i],nums[twoIndex]);
+                twoIndex--;
             }
             else if(nums[i] == 0){
-                swap(nums[i],nums[start]);
-                start++;
+                swap(nums[i],nums[zeroIndex]);
+                zeroIndex++;
                 i++;
             }
             else {
