8df3724c53b05964a0706dba44aa0ca6ff372227
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-07-28 15:07:50 -0400

    Time: 3 ms (37.66%), Space: 42 MB (34.42%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> prefixSum(nums.size());
-        prefixSum[0]=1;
-        for(int i=1;i<nums.size();i++){
-            prefixSum[i] = prefixSum[i-1]*nums[i-1];
+        vector<int> leftProduct(nums.size(),1);
+        leftProduct[0] = nums[0];
+        for(int i = 1;i<nums.size();i++){
+            leftProduct[i] = leftProduct[i-1] * nums[i];
         }
-        vector<int> suffixSum(nums.size());
-        suffixSum[nums.size()-1]=1;
-        for(int i=nums.size()-2;i>=0;i--){
-            suffixSum[i] = suffixSum[i+1]*nums[i+1];
+        // get right product!
+        vector<int> rightProduct(nums.size(),1);
+        rightProduct[nums.size()-1] = nums[nums.size()-1];
+        for(int i = nums.size()-2;i>=0;i--){
+            rightProduct[i] = rightProduct[i+1] * nums[i];
         }
-        vector<int> ans(nums.size());
-        for(int i=0;i<nums.size();i++){
-            ans[i] = prefixSum[i]*suffixSum[i];
+        vector<int>ans(nums.size(),0);
+        ans[0] = rightProduct[1];
+        ans[nums.size()-1] = leftProduct[nums.size()-2];
+        for(int i =1;i<nums.size()-1;i++){
+            ans[i] = leftProduct[i-1] * rightProduct[i+1];
         }
         return ans;
     }
