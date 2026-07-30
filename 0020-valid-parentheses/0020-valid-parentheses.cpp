d57aa0c94e1242e92cad199aaf8dd11dd10686af
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-07-30 17:57:02 -0400

    Time: 0 ms (100%), Space: 8.7 MB (98.59%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -3,24 +3,27 @@ public:
     bool isValid(string s) {
         stack<char>st;
         for(int i = 0;i<s.size();i++){
-            char ch = s[i];
-            if(ch == '(' || ch == '{' || ch == '['){
-                st.push(ch);
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                st.push(s[i]);
+                continue;
             }
-            else {
-                if(st.empty()){
+            else if(s[i] == ')'){
+                if(st.empty() || st.top()!= '('){
                     return false;
                 }
-                char top = st.top();
-
-                if ((ch == ')' && top != '(') ||
-                    (ch == '}' && top != '{') ||
-                    (ch == ']' && top != '[')) {
+            }
+            else if(s[i] == ']'){
+                if(st.empty() || st.top()!= '['){
                     return false;
                 }
-                st.pop();
             }
+            else if(s[i] == '}'){
+                if(st.empty() || st.top()!= '{'){
+                    return false;
+                }
+            }
+            st.pop();
         }
-        return st.empty();
+        return st.empty() ? true : false;
     }
 };
