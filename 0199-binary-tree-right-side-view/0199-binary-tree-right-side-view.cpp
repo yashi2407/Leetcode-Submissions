c464071dd58a7391f29309f5cc9d3d089085160f
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-10 20:09:10 -0400

    Time: 0 ms (100%), Space: 14.7 MB (96.17%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,29 +11,20 @@
  */
 class Solution {
 public:
+    void util(TreeNode* root,vector<int> &ans, int level){
+        if(root == NULL){
+            return;
+        }
+        // push the answer if valid
+        if(ans.size() == level){
+            ans.push_back(root->val);
+        }
+        util(root->right,ans,level+1);
+        util(root->left,ans,level+1);
+    }
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        queue<TreeNode*>q;
-        if(root){
-            q.push(root);
-        }
-        while(!q.empty()){
-            TreeNode* node = q.back();
-            ans.push_back(node->val);
-            int n = q.size();
-            for(int i=0;i<n;i++){
-                TreeNode* node = q.front();
-                q.pop();
-                if(node->left){
-                    q.push(node->left);
-                }
-                if(node->right){
-                    q.push(node->right);
-                }
-
-            }
-
-        }
+        util(root,ans,0);
         return ans;
     }
 };
