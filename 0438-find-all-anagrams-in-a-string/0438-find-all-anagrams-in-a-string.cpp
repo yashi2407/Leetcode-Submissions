c9b3caa0b639e0ccf9a6eedeb86a108474b345f1
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-12 16:13:25 -0400

    Time: 4 ms (67.7%), Space: 11.6 MB (63.66%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        int k = p.size();
         vector<int> ans;
-        int i=0,j=0;
-        map<char,int>mp;
+        unordered_map<char,int>mp;
         for(int i = 0;i<p.size();i++){
             mp[p[i]]++;
         }
-        int count = mp.size();
+        int distinctCount = mp.size();
+        int i=0;int j=0;
         while(j<s.size()){
-            if(mp.find(s[j])!= mp.end()){
+            if(mp.find(s[j]) != mp.end()){
                 mp[s[j]]--;
                 if(mp[s[j]] == 0){
-                   count--;
+                    distinctCount--;
                 }
             }
-            if(j-i+1 == k){
-                if(count==0){
+            // we hit the window size!
+            if(j-i+1 == p.size()){
+                if(distinctCount == 0){
                     ans.push_back(i);
                 }
-                // revert calcualtions of i
-                // if the current letter in p, the increase the count!
-                if(mp.find(s[i])!= mp.end()){
+                // we hit thw window size, gotta reduce i
+                if(mp.find(s[i]) != mp.end()){
                     mp[s[i]]++;
                     if(mp[s[i]] == 1){
-                        count++;
+                        distinctCount++;
                     }
                 }
                 i++;
