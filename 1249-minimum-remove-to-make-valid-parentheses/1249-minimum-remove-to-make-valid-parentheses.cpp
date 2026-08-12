60d98f9f6ecadc2df2b19c4f2ccab7fc9f1c9246
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-12 14:25:09 -0400

    Time: 14 ms (20.27%), Space: 14.5 MB (53.44%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        stack<int>st;
+        set<int>indexToSkip;
+        string ans = "";
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '('){
+                // open bracket at i index!
+                st.push(i);
+            }
+            else if(s[i] == ')'){
+                if(st.empty()){
+                    // gotta skip the index
+                    indexToSkip.insert(i);
+                }
+                else {
+                    st.pop();
+                }
+            }
+        }
+        // take out all open brackets that we need to remove
+        while(!st.empty()){
+            indexToSkip.insert(st.top());
+            st.pop();
+        }
+        for(int i=0;i<s.size();i++){
+            if(indexToSkip.find(i) == indexToSkip.end()){
+                ans+= s[i];
+            }
+        }
+        return ans;
+    }
+};
