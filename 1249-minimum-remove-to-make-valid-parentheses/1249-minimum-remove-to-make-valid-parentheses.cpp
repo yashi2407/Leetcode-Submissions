2997ea05e19c1a7b7343ef93db3489654e35bc6a
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-12 15:06:40 -0400

    Time: 12 ms (28.28%), Space: 14.6 MB (33.88%) - LeetHub

--- 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -1,34 +1,39 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
-        stack<int>st;
-        unordered_set<int>indexToSkip;
+        int openCounter = 0;
         string ans = "";
         for(int i=0;i<s.size();i++){
             if(s[i] == '('){
-                // open bracket at i index!
-                st.push(i);
+                openCounter++;
+                ans += s[i];
             }
             else if(s[i] == ')'){
-                if(st.empty()){
-                    // gotta skip the index
-                    indexToSkip.insert(i);
+                if(openCounter > 0){
+                    openCounter--;
+                    ans += s[i];
+                }
+            }
+            else {
+                ans += s[i];
+            }
+        }
+        // reverse(ans.begin(),ans.end());
+        if(openCounter == 0){
+            return ans;
+        }
+        string finalAns = "";
+        if(openCounter>0){
+            for(int i = ans.size()-1;i>=0;i--){
+                if(ans[i] == '(' && openCounter>0){
+                    openCounter--;
                 }
                 else {
-                    st.pop();
+                    finalAns += ans[i];
                 }
             }
         }
-        // take out all open brackets that we need to remove
-        while(!st.empty()){
-            indexToSkip.insert(st.top());
-            st.pop();
-        }
-        for(int i=0;i<s.size();i++){
-            if(indexToSkip.find(i) == indexToSkip.end()){
-                ans+= s[i];
-            }
-        }
-        return ans;
+        reverse(finalAns.begin(), finalAns.end());
+        return finalAns;
     }
 };
