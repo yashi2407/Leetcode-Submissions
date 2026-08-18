ac000778f0874fb7f206d0cc6ab6eafd9033e405
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-18 17:22:36 -0400

    Time: 0 ms (100%), Space: 22.9 MB (26.62%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -1,32 +1,66 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    void reorderList(ListNode* head) {
-        if (!head || !head->next) return;
-
-        stack<ListNode*> st;
-        for (ListNode* t = head; t != nullptr; t = t->next) st.push(t);
-
-        ListNode* C1 = head;
-
-        while (C1) {
-            ListNode* C2 = st.top(); 
-            st.pop();
-            ListNode* nextElement = C1->next;
-            // If we've met/crossed the middle (odd/even), terminate and stop.
-            if (C2 == C1) {                 // odd length: middle node
-                C1->next = nullptr;
-                break;
-            }
-            if (C2 == nextElement) {        // even length: two middle nodes
-                C1->next = C2;
-                C2->next = nullptr;
-                break;
-            }
-
-            // Normal weave: C1 -> C2 -> nextElement
-            C1->next = C2;
-            C2->next = nextElement;
-            C1 = nextElement;
+        ListNode* reverseList(ListNode* head) {
+        ListNode* prev= NULL;
+        ListNode* current= head;
+        if(head==NULL){
+            return NULL;
         }
+        if(head->next==NULL){
+            return head;
+        }
+        ListNode* next = head->next;
+
+        while(current!= NULL){
+            current->next = prev;
+            prev = current;
+            current=next;
+            if(next){
+                next = next->next;
+            }
+        }
+        return prev;
+        
+    }
+    void reorderList(ListNode* head) {
+        ListNode* temp = head;
+        int length = 0;
+        while(temp!= NULL){
+            length++;
+            temp = temp->next;
+        }
+        int splitIndex = (length+1)/2;
+        ListNode* head2 = head;
+        ListNode* prev = NULL;
+        while(splitIndex!=0){
+            prev = head2;
+            head2 = head2->next;
+            splitIndex--;
+        }
+        prev->next = NULL;
+        ListNode *newHead = reverseList(head2);
+        ListNode* first = head;
+        ListNode* second = newHead;
+        while (second != NULL) {
+            ListNode* firstNext = first->next;
+            ListNode* secondNext = second->next;
+
+            first->next = second;
+            second->next = firstNext;
+
+            first = firstNext;
+            second = secondNext;
+        }
+
     }
 };
