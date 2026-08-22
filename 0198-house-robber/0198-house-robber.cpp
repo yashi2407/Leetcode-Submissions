c5667b9c00539c28bcceab84d03a177a63e450bc
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-22 11:31:02 -0400

    Time: 0 ms (100%), Space: 10.7 MB (24.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public: 
-    int util(vector<int>& nums, int currentIndex, int lastIndex, vector<vector<int>>&dp){
+    int util(vector<int>& nums, int currentIndex,vector<int>&dp){
         if(currentIndex>=nums.size()){
             return 0;
         }
         // at this index, either i loot it or not
-        if(dp[currentIndex][lastIndex+1]!=-1){
-            return dp[currentIndex][lastIndex+1];
+        if(dp[currentIndex] !=-1){
+            return dp[currentIndex];
         }
-        int take = 0;
-        int notTake = 0;
-        if(lastIndex == -1 || lastIndex != currentIndex-1){
-            take = nums[currentIndex] + util(nums,currentIndex+1,currentIndex,dp);
-        }
-        notTake = util(nums,currentIndex+1,lastIndex,dp);
-        return dp[currentIndex][lastIndex+1] = max(take,notTake);
+        int take = nums[currentIndex] + util(nums,currentIndex+2,dp);;
+        int notTake = util(nums,currentIndex+1,dp);;
+        return dp[currentIndex] = max(take,notTake);
     }
     int rob(vector<int>& nums) {
-        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return util(nums,0,-1,dp);
+        vector<int>dp(nums.size()+2,-1);
+        return util(nums,0,dp);
     }
 };
