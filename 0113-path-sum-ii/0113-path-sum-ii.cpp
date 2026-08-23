b906d475b8f55f40c38ce105b373806ee553e905
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-23 13:15:09 -0400

    Time: 19 ms (8.05%), Space: 40.6 MB (7.98%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,29 +11,29 @@
  */
 class Solution {
 public:
-    void hasSum(TreeNode* root, int targetSum,vector<vector<int>>&ans,vector<int>&currentPath){
+    void util(TreeNode* root, int targetSum,vector<vector<int>>&ans,vector<int>currentPath){
         if(root == NULL){
             return;
         }
+        // leaf node
         if(root->left == NULL && root->right == NULL){
-            if(targetSum - root->val == 0){
+            if(root->val == targetSum){
                 currentPath.push_back(root->val);
                 ans.push_back(currentPath);
                 currentPath.pop_back();
+                return;
             }
-            return;
         }
         currentPath.push_back(root->val);
-        hasSum(root->left,targetSum-root->val,ans,currentPath);
-        hasSum(root->right,targetSum-root->val,ans,currentPath);
+        targetSum = targetSum-root->val;
+        util(root->left,targetSum,ans,currentPath);
+        util(root->right,targetSum,ans,currentPath);
         currentPath.pop_back();
-        return;
-
     }
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>>ans;
         vector<int>currentPath;
-        hasSum(root,targetSum,ans,currentPath);
+        util(root,targetSum,ans,currentPath);
         return ans;
     }
 };
