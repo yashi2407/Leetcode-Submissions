6a7546fd8e3ffbfc91fe070e25d5154e71c3e81c
Yashika Ahlawat <60531590+yashi2407@users.noreply.github.com>
2026-08-25 22:27:41 -0400

    Time: 0 ms (100%), Space: 8.4 MB (56.1%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -1,44 +1,50 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        vector<int>count(26);
-        for(int i = 0;i<s.size();i++){
-           count[s[i]-'a']++;
+        string ans="";
+        unordered_map<char,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+            if(mp[s[i]] == ((s.size()+1)/2)+1){
+                return "";
+            }
         }
+        // put them in heap!!! greddily will set 
         priority_queue<pair<int,char>>pq;
-        for(int i = 0;i<count.size();i++){
-            if(count[i]!=0){
-                pq.push({count[i], i +'a'});
-            }  
+        for(auto &it:mp){
+            pq.push({it.second,it.first});
         }
-        string res = "";
+        
         while(pq.size()>1){
-            auto t1 = pq.top();
-            pq.pop();
-            auto t2 = pq.top();
+            char c1 = pq.top().second;
+            int count1 = pq.top().first;
             pq.pop();
 
-            res += t1.second;
-            res += t2.second;
-            t1.first--;
+            char c2 = pq.top().second;
+            int count2 = pq.top().first;
+            pq.pop();
 
-            if(t1.first>0){
-                pq.push(t1);
+            ans+=c1;
+            ans+=c2;
+
+            count1--;
+            count2--;
+            if(count1 > 0){
+                pq.push({count1,c1});
             }
-            t2.first--;
-            if(t2.first>0){
-                pq.push(t2);
+            if(count2 > 0){
+                pq.push({count2,c2});
             }
         }
-        // if there's one more element present in pq
+        // out of this loop, either same character multiple times or 1 extra
         if(!pq.empty()){
-            if(pq.top().first > 1){
+            if(pq.top().first>1){
                 return "";
             }
             else{
-                res+=pq.top().second;
+                ans+= pq.top().second;
             }
         }
-        return res;
+        return ans;
     }
 };
